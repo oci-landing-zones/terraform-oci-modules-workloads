8b86aedb44c46a344ba8c44e089f77b5dcae665f
Andre Correa <andre.correa@oracle.com>
2024-05-09 14:38:59 -0300

    feat: default_ssh_public_key_path and ssh_public_key_path are overloaded, taking a key string in addition to a key file.

--- cis-compute-storage/compute.tf
+++ cis-compute-storage/compute.tf
@@ -170,7 +170,7 @@ resource "oci_core_instance" "these" {
       }
     }
     metadata = {
-      ssh_authorized_keys = each.value.ssh_public_key_path != null ? file(each.value.ssh_public_key_path) : file(var.instances_configuration.default_ssh_public_key_path)
+      ssh_authorized_keys = each.value.ssh_public_key_path != null ? (fileexists(each.value.ssh_public_key_path) ? file(each.value.ssh_public_key_path) : each.value.ssh_public_key_path) : var.instances_configuration.default_ssh_public_key_path != null ? (fileexists(var.instances_configuration.default_ssh_public_key_path) ? file(var.instances_configuration.default_ssh_public_key_path) : var.instances_configuration.default_ssh_public_key_path): null
       user_data           = contains(keys(data.template_file.cloud_config),each.key) ? base64encode(data.template_file.cloud_config[each.key].rendered) : null
     }
 }
