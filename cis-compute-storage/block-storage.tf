29b0bab6c421c361b39b7615d088ba587f93b03d
Peter Hrvola <peter.hrvola@oracle.com>
2026-07-22 09:38:50 +0200

    Fix #46: Block volume restore - restore from volumeBackup

--- cis-compute-storage/block-storage.tf
+++ cis-compute-storage/block-storage.tf
@@ -76,6 +76,13 @@ resource "oci_core_volume" "these" {
       display_name        = "${each.value.display_name}-replica"
     }
   }
+  dynamic "source_details" {
+    for_each = each.value.source != null ? [each.value.source] : []
+    content {
+      type = source_details.value.type
+      id   = source_details.value.id
+    }
+  }
   defined_tags  = each.value.defined_tags != null ? each.value.defined_tags : var.storage_configuration.default_defined_tags
   freeform_tags = merge(local.cislz_module_tag, each.value.freeform_tags != null ? each.value.freeform_tags : var.storage_configuration.default_freeform_tags)
 }
